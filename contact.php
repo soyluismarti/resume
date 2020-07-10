347b9cb1ac99f929c44d26463a8a2dda4d78da33
Luis Martinez <hello@soyluismarti.com>
2020-07-10 19:49:54 -0400

    1
    1

--- contact.php
+++ contact.php
@@ -1,3 +1,16 @@
 <?php
+if(isset($_POST)['submit']))){
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$mailFrom = $_POST['mail'];
+$message = $_POST['message'];
+  
 
-if 
+$mailTo="luisalfonsomar@gmail.com";
+$headers = "From:". $mailFrom; 
+$txt ="You recceived a message in your website from".$name.".\n\n ". $message".\n\n ".$phone;
+mail(
+$mailTo, $tel, $txt, $headers);
+header("Location: home.html");
+
+}
